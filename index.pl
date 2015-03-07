a904be9b86f3338e1be791a29f49f5eff8f48fed
Zoffix Znet <cpan@zoffix.com>
2015-03-06 21:09:02 -0500

    Added display of common stops

--- index.pl
+++ index.pl
@@ -7,10 +7,22 @@ use lib '/home/zoffix/perl5/lib/perl5/';
 
 use HTML::Template;
 use BT::NextRide;
-use HTML::Entities;
-my $bt = BT::NextRide->new( stop => 3114 )->get_next_ride;
 
-print "Content-type: text/html\n\n";
-print $bt;
+my @STOPS_TO_DISPLAY = qw/3143  3114  1768  1506  2650/;
 
-# print "</pre>\n";
+my $bt = BT::NextRide->new;
+
+my $t = HTML::Template->new_file( 'template.html', die_on_bad_params => 0 );
+$t->param(
+    stops => [
+        map +{
+            stop  => $_,
+            times => [
+                map +{ time => $_ },
+                    $bt->set_stop( $_ )->get_next_ride,
+            ],
+        }, @STOPS_TO_DISPLAY,
+    ],
+);
+
+print "Content-type: text/html\n\n", $t->output;
